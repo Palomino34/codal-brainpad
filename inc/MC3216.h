5cb654b6cae41c8d0eca923c7cee18fdeed3f091
Dat Tran <dat.tran@ghielectronics.com>
2021-08-02 13:39:36 -0400

    change accel

--- inc/MC3216.h
+++ inc/MC3216.h
@@ -1,41 +1,20 @@
-#ifndef MMA8453_H
-#define MMA8453_H
+#ifndef MC3216_H
+#define MC3216_H
 
 #include "CoordinateSystem.h"
 #include "Pin.h"
 #include "I2C.h"
 #include "Accelerometer.h"
 
-#define MMA8453_DEFAULT_ADDR  0x38
+#define MC3216_DEFAULT_ADDR  (0x4C<<1)
 
-#define STATUS_REG            0x00
-#define STATUS_REG_AUX        0x07
-#define OUT_X_MSB             0x01
-#define OUT_X_LSB             0x02
-#define OUT_Y_MSB             0x03
-#define OUT_Y_LSB             0x04
-#define OUT_Z_MSB             0x05
-#define OUT_Z_LSB             0x06
-
-#define CTRL_REG1             0x2A
-#define CTRL_REG2             0x2B
-#define CTRL_REG3             0x2C
-#define CTRL_REG4             0x2D
-#define CTRL_REG5             0x2E
-#define TRANSIENT_CFG         0x1D
-#define TRANSIENT_THS         0x1F
-#define TRANSIENT_COUNT       0x20
-#define HP_FILTER_CUTOFF      0x0F
-#define FT_MT_THS             0x17
-#define FF_MT_COUNT           0x18
-#define FF_MT_CFG             0x15
-#define XYZ_DATA_CFG          0x0E
-#define CTRL_REG1_SLEEP       0x00
-#define CTRL_REG2_RESET       0x40
-#define CTRL_REG2_HIGH_RES    0x01
-#define CTRL_REG4_INT_DATA    0x01
-#define CTRL_REG5_SET_INT_PIN 0x01
-#define CTRL_REG1_ACTIVE      0x01
+const int MC3216_Mode = 0x07;
+const int MC3216_Opstat = 0x04;
+const int MC3216_Outcfg = 0x20;
+const int MC3216_XOut = 0x0D;
+const int MC3216_YOut = 0x0F;
+const int MC3216_ZOut = 0x11;
+const int MC3216_SRTFR = 0x08;
 
 
 namespace codal {
@@ -46,9 +25,10 @@ namespace codal {
         bool 			  mylocker;
 
         void writeRegister(uint8_t reg, uint8_t val);
+		unsigned long current_ms;
 
     public:
-        MC3216(codal::I2C& _i2c, Pin& int1, CoordinateSpace& coordinateSpace, uint16_t address = MMA8453_DEFAULT_ADDR, uint16_t id = DEVICE_ID_ACCELEROMETER);
+        MC3216(codal::I2C& _i2c, Pin& int1, CoordinateSpace& coordinateSpace, uint16_t address = MC3216_DEFAULT_ADDR, uint16_t id = DEVICE_ID_ACCELEROMETER);
         ~MC3216();
 
         int updateSample();
--- source/MC3216.cpp
+++ source/MC3216.cpp
@@ -5,27 +5,31 @@
 #include "CodalFiber.h"
 #include "platform/mbed_wait_api.h"
 
+#include "BrainPad.h"
+#include "Timer.h"
+
 using namespace codal;
 
-static const KeyValueTableEntry rangeRegisterData[] = {
-    { 2, 0x00 },
-    { 4, 0x01 },
-    { 8, 0x02 },
-};
+// static const KeyValueTableEntry rangeRegisterData[] = {
+    // { 2, 0x00 },
+    // { 4, 0x01 },
+    // { 8, 0x02 },
+// };
 
-static const KeyValueTableEntry rangeDivisorData[] = {
-    { 2, 256 },
-    { 4, 128 },
-    { 8, 64 },
-};
+// static const KeyValueTableEntry rangeDivisorData[] = {
+    // { 2, 256 },
+    // { 4, 128 },
+    // { 8, 64 },
+// };
 
-CREATE_KEY_VALUE_TABLE(rangeRegister, rangeRegisterData);
-CREATE_KEY_VALUE_TABLE(rangeDivisor, rangeDivisorData);
+// CREATE_KEY_VALUE_TABLE(rangeRegister, rangeRegisterData);
+// CREATE_KEY_VALUE_TABLE(rangeDivisor, rangeDivisorData);
 
 MC3216::MC3216(codal::I2C& _i2c, Pin& _int1, CoordinateSpace& coordinateSpace, uint16_t address, uint16_t id) : Accelerometer(coordinateSpace, id), i2c(_i2c), int1(_int1) {
 
     this->address = address;
 	this->mylocker = false;
+	this->current_ms = system_timer_current_time();
 
     configure();
 }
@@ -54,35 +58,53 @@ int MC3216::updateSample() {
 
 		return DEVICE_OK;
 	}
-
-	int divisor = rangeDivisor.get(this->getRange());
-    uint8_t data[6];
 	
+		
 	if (&i2c == nullptr)
 		return DEVICE_OK;
+
+	
+	
+	
+   
+	
     
 
-    if (int1.getDigitalValue() == 0) {
-        i2c.readRegister(address, OUT_X_MSB, data, 6);
+    if (system_timer_current_time() - 100 > this->current_ms) {
+		
+		uint8_t dataX[1];
+		uint8_t dataY[1];
+		uint8_t dataZ[1];
+		 
+        i2c.readRegister(address, MC3216_XOut, dataX, 1);		
+		int32_t x = dataX[0];
+		
+		i2c.readRegister(address, MC3216_YOut, dataY, 1);		
+		int32_t y = dataY[0];
+		
+		i2c.readRegister(address, MC3216_ZOut, dataZ, 1);		
+		int32_t z = dataZ[0];
 
-        int32_t x = (data[0] << 2) | (data[1] >> 6);
-        int32_t y = (data[2] << 2) | (data[3] >> 6);
-        int32_t z = (data[4] << 2) | (data[5] >> 6);
+        // int32_t x = (data[0] << 2) | (data[1] >> 6);
+        // int32_t y = (data[2] << 2) | (data[3] >> 6);
+        // int32_t z = (data[4] << 2) | (data[5] >> 6);
 
-        if (x >= 512) x -= 1024;
-        if (y >= 512) y -= 1024;
-        if (z >= 512) z -= 1024;
+        // if (x >= 512) x -= 1024;
+        // if (y >= 512) y -= 1024;
+        // if (z >= 512) z -= 1024;
 
-        x *= 1024;
-        x /= divisor;
+        // x *= 1024;
+        // x /= divisor;
 
-        y *= 1024;
-        y /= divisor;
+        // y *= 1024;
+        // y /= divisor;
 
-        z *= 1024;
-        z /= divisor;
+        // z *= 1024;
+        // z /= divisor;
 		this->mylocker = true;
         update({ -y, x, -z }); //To transform to ENU
+				
+		this->current_ms = system_timer_current_time();
 		this->mylocker = false;
     }
 
@@ -90,12 +112,22 @@ int MC3216::updateSample() {
 }
 
 int MC3216::configure() {
-    writeRegister(CTRL_REG1, CTRL_REG1_SLEEP);
+    // writeRegister(CTRL_REG1, CTRL_REG1_SLEEP);
 
-    writeRegister(XYZ_DATA_CFG, rangeRegister.get(getRange()));
-    writeRegister(CTRL_REG4, CTRL_REG4_INT_DATA);
-    writeRegister(CTRL_REG5, CTRL_REG5_SET_INT_PIN);
-    writeRegister(CTRL_REG1, CTRL_REG1_ACTIVE);
+    // writeRegister(XYZ_DATA_CFG, rangeRegister.get(getRange()));
+    // writeRegister(CTRL_REG4, CTRL_REG4_INT_DATA);
+    // writeRegister(CTRL_REG5, CTRL_REG5_SET_INT_PIN);
+    // writeRegister(CTRL_REG1, CTRL_REG1_ACTIVE);
+	
+	uint8_t data[1];
+	
+	writeRegister(MC3216_Outcfg, 2);
+	writeRegister(MC3216_Mode, 1);
+	
+	i2c.readRegister(address, MC3216_Opstat, data, 1);
+	
+	if ((data[0] & 0x1) != 0x1 )
+		while(1);
 
     return DEVICE_OK;
 }
